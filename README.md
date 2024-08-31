4bb3ccd492f7589678b4633b76860411a43633b9
Kopernikus73 <kopernikus3.141@gmail.com>
2024-08-31 13:56:42 +0200

    1.0.0

--- README.md
+++ README.md
@@ -2,12 +2,44 @@
 
 ---
 ## How to use
-blah blah
 
+1. Generate a sudoku with standard difficulty
+    
+    `$ ./sudoku-generator`
+2. Generate a sudoku with changed difficulty [0..5]
+
+   `$ ./sudoku-generator -d 3` (The difficulty is only relative because it is based on randomness)
+3. The generated sudokus will be in the ***./sudoku/*** directory 
+    
+    There will be a completed version at the top and a version with missing numbers, based on the difficulty, on the bottom
+    1. Remove the solved sudoku
+   
+       `$ ./sudoku-generator -s` (You can still change the difficulty)
 ---
 ## Installation
-blah blah
+### Executable Download
+Click on the Releases tab and download the system specific executable file
 
+### Manual Download (not recommended)
+1. Install [cargo on the website for Windows](https://doc.rust-lang.org/cargo/getting-started/installation.html) or through your package manger on Linux
+    Don't know which package manager you use?
+   1. If [neofetch](https://github.com/dylanaraps/neofetch) is installed:
+    
+        `$ neofetch` 
+   2. Else look [here](https://en.wikipedia.org/wiki/List_of_software_package_management_systems) and see if you recognize any of these names and try out to use it
+   3. Look it up online
+2. Clone the repository
+
+    `$ git clone https://github.com/Kopernikus73/sudoku`
+
+3. Navigate into the directory of the cloned repository
+   
+    `$ cd your/path/sudoku`
+4. Build the executable
+    `$ cargo build --release`
+    
+   The executable file will be in the ***./target/release/*** directory and is named ***sudoku-generator***
+5. Now you can follow the instructions in the [How to Use](#how-to-use) section
 ---
 ## License
-Licence is [GPL-3](https://github.com/Kopernikus73/sudoku/blob/main/LICENSE)
+Licence is [GPL-3.0](https://github.com/Kopernikus73/sudoku/blob/main/LICENSE)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,124 @@
+use std::env;
+use rand::Rng;
+use std::fs::File;
+use std::io::Write;
+
+#[derive(Clone)]
+struct Coordinate {
+    row: u8,
+    column: u8,
+    number: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut difficulty: u8 = 1;
+    let mut solved = true;
+
+    for (index, argument) in args.iter().enumerate().skip(1) {
+        match argument.as_str() {
+            "-h" | "--help" => {
+                println!("\n\x1b[1m./sudoku-generator \x1b[0m\x1b[2m[-option]\x1b[0m\x1b[1m <argument>\x1b[0m\n");
+                println!("\x1b[2moptions\x1b[0m:
+  -h -> shows infos about this app
+  -d -> changes difficulty [1..5] std: 1 (./sudoku-generator -d 3)");
+                std::process::exit(0);
+            }
+            "-d" => {
+                if args.len() <= index+1{
+                    eprintln!("\x1b[31mmissing argument\x1b[0m");
+                    std::process::exit(1);
+                }else if ["1","2","3","4","5"].contains(&&*args[index + 1]){
+                    difficulty = (&&*args[index + 1]).parse::<u8>().unwrap()
+                }else{
+                    eprintln!("Wrong argument");
+                    std::process::exit(1);
+                }
+            }
+            "-s" => {
+               solved = false;
+            }
+            _ => {}
+        }
+    }
+
+
+    let mut field: Vec<Coordinate> = vec![];
+
+    for i in 1..=9 {
+        for k in 1..=9 {
+            field.push(Coordinate {
+                row: i,
+                column: k,
+                number: 0,
+            });
+        }
+    }
+
+    let mut rng = rand::thread_rng();
+    let mut wdh: u16 = 0;
+
+    while field.iter().any(|coord| coord.number == 0) {
+        for idx in 0..field.len() {
+            let num = rng.gen_range(1..=9);
+            let row_start = (field[idx].row as usize - 1) * 9;
+            let column_start = field[idx].column as usize - 1;
+
+            let row_conflict = (0..9).any(|i| field[row_start + i].number == num);
+            let column_conflict = (0..9).any(|i| field[column_start + i * 9].number == num);
+
+            if !row_conflict && !column_conflict {
+                field[idx].number = num;
+            }
+        }
+        wdh += 1;
+        if wdh == 4095{
+            break
+        };
+    }
+
+    let mut file_name = "sudokus/sudoku".to_string();
+    for _ in 0..9{
+        file_name += format!("{}",rng.gen_range(0..=9)).as_str()
+    }
+    file_name += format!("-{}",difficulty).as_str();
+
+    let mut file = File::create(file_name).expect("Unable to create file");
+
+    if solved == true {
+        for coord in &field {
+            write!(file, " {}", coord.number).expect("Unable to write to file");
+            if coord.column % 3 == 0 && coord.column != 9 && coord.column != 0 {
+                write!(file, " |").expect("Unable to write to file");
+            }
+            if coord.column == 9 {
+                writeln!(file).expect("Unable to write to file");
+                if coord.row % 3 == 0 && coord.row != 0 && coord.row != 9 {
+                    writeln!(file, " ------|-------|------").expect("Unable to write to file");
+                }
+            }
+        }
+        writeln!(file).expect("Unable to write to file");
+        writeln!(file).expect("Unable to write to file");
+    }
+
+    let mut rng = rand::thread_rng();
+
+    for coord in &field {
+        let show = rng.gen_range(0..difficulty+1);
+        if show == 1 {
+            write!(file, " {}", coord.number).expect("Unable to write to file");
+        } else {
+            write!(file, "  ").expect("Unable to write to file");
+        }
+        if coord.column % 3 == 0 && coord.column != 9 && coord.column != 0 {
+            write!(file, " |").expect("Unable to write to file");
+        }
+        if coord.column == 9 {
+            writeln!(file).expect("Unable to write to file");
+            if coord.row % 3 == 0 && coord.row != 0 && coord.row != 9 {
+                writeln!(file, " ------|-------|------").expect("Unable to write to file");
+            }
+        }
+    }
 }
