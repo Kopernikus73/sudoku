be1f4297c114760547b3948a7c9c878eb924fd6e
Kopernikus73 <kopernikus3.141@gmail.com>
2024-09-01 15:23:21 +0200

    1.0.3

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,13 @@ fn main() {
     let mut difficulty: u8 = 1;  // Difficulty of the generated sudoku [1..5]
     let mut amount: u8 = 1;      // Amount of generated sudokus [1..10]
     let mut solved = true;  // If the generated sudoku should include a solved version [true|false]
+    let mut json = false;   // If the generated sudoku should be also exported in JSON format [true|false]
 
     for (index, argument) in args.iter().enumerate().skip(1) {
         match argument.as_str() {
             "-h" | "--help" => {
                 println!("\n\x1b[1m./sudoku-generator \x1b[0m\x1b[33m[-option]\x1b[0m\x1b[1m <argument>\x1b[0m    \x1b[2m(Can be empty and repeated)\x1b[0m\n");
-                println!("\x1b[33moptions\x1b[0m:\n  \x1b[33m-h\x1b[0m -> shows infos about this app\n  \x1b[33m-d\x1b[0m -> changes difficulty [1..10] std: 1 (./sudoku-generator -d 3)\n  \x1b[33m-s\x1b[0m -> show only solved sudoku (./sudoku-generator -s)\n  \x1b[33m-a\x1b[0m -> changes the amount of genrated Sudokus [1..10] std: 1 (./sudoku-generator -a 3) \x1b[2m((Creates <amount> times of files))\x1b[0m\n");
+                println!("\x1b[33moptions\x1b[0m:\n  \x1b[33m-h\x1b[0m -> shows infos about this app\n  \x1b[33m-d\x1b[0m -> changes difficulty [1..10] std: 1 (./sudoku-generator -d 3)\n  \x1b[33m-s\x1b[0m -> show only solved sudoku (./sudoku-generator -s)\n  \x1b[33m-a\x1b[0m -> changes the amount of genrated Sudokus [1..10] std: 1 (./sudoku-generator -a 3) \x1b[2m((Creates <amount> times of files))\x1b[0m\n  \x1b[33m-j\x1b[0m -> creates an extra json file with information about the sudoku\n");
 
                 std::process::exit(0);
             }
@@ -49,12 +50,14 @@ fn main() {
                     std::process::exit(1);
                 }
             }
+            "-j" => {
+                json = true;
+            }
             _ => {}
         }
     }
 
     for _ in 0..amount {
-
         let mut field: Vec<Coordinate> = vec![]; // Sudoku Field
 
         for i in 1..=9 {
@@ -76,25 +79,41 @@ fn main() {
                 let row_start = (field[idx].row as usize - 1) * 9;
                 let column_start = field[idx].column as usize - 1;
 
+                // Calculate the top-left coordinate of the 3x3 block
+                let block_row_start = (field[idx].row as usize - 1) / 3 * 3;
+                let block_column_start = (field[idx].column as usize - 1) / 3 * 3;
+
+                // Check for conflicts in the 3x3 block
+                let block_conflict = (0..3).any(|i| {
+                    (0..3).any(|j| {
+                        let block_index = (block_row_start + i) * 9 + (block_column_start + j);
+                        field[block_index].number == num
+                    })
+                });
+
                 let row_conflict = (0..9).any(|i| field[row_start + i].number == num);
                 let column_conflict = (0..9).any(|i| field[column_start + i * 9].number == num);
 
-                if !row_conflict && !column_conflict {
+                if !row_conflict && !column_conflict && !block_conflict {
                     field[idx].number = num;
                 }
             }
             wdh += 1;
-            if wdh == 4095{
-                break
-            };
+            if wdh == 65535 {
+                break;
+            }
         }
+
+
 
         let mut file_name = "sudokus/sudoku".to_string();
         for _ in 0..9{
-            let added_num: u8 = rng.gen_range(0..=9);
+            let added_num: u8 = rng.gen_range(1..=9);
             file_name += format!("{}",added_num).as_str();
         }
         file_name += format!("-{}",difficulty).as_str();
+        let mut json_file_name = file_name.clone();
+        file_name += ".txt";
 
         match fs::create_dir_all("/sudokus") {
             Ok(_) => {},
@@ -137,6 +156,31 @@ fn main() {
                     writeln!(file, " ------|-------|------").expect("Unable to write to file");
                 }
             }
+        }
+
+        if json == true {
+            json_file_name += ".json";
+            let num = &json_file_name.clone()[14..23];
+            let mut json_file = File::create(json_file_name).expect("Unable to create file");
+
+            writeln!(json_file, "{}\n  \"difficulty\": {},\n  \"id\": {},\n  \"field\": {}","{",difficulty,num,"{").expect("Unable to write to file");
+            for coord in &field {
+                if coord.column == 1{
+                    write!(json_file,"    \"row{}\": [",coord.row).expect("Unable to write to file");
+                }
+                if coord.column != 9 {
+                    write!(json_file, "{},", coord.number).expect("Unable to write to file");
+                }else{
+                    write!(json_file, "{}", coord.number).expect("Unable to write to file");
+                }
+                if coord.column == 9{
+                    write!(json_file,"]").expect("Unable to write to file");
+                    if coord.row != 9{
+                        writeln!(json_file,",").expect("Unable to write to file");
+                    }
+                }
+            }
+            writeln!(json_file, "{}","\n  }\n}").expect("Unable to write to file");
         }
     }
 }
